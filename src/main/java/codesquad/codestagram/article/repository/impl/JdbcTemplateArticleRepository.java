52f2f0a717c121ec07f07637136ed62323c1b369
jaehoonchoi <eeoos8840@gmail.com>
2025-03-25 23:53:27 +0900

    Feat: JdbcTemplateUserRepository, JdbcTemplateArticleRepository 구현체 생성
    - 이 구현체들을 사용하도록 springConfig 클래스 수정

---
+++ src/main/java/codesquad/codestagram/article/repository/impl/JdbcTemplateArticleRepository.java
@@ -0,0 +1,62 @@
+package codesquad.codestagram.article.repository.impl;
+
+import codesquad.codestagram.article.domain.Article;
+import codesquad.codestagram.article.repository.ArticleRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class JdbcTemplateArticleRepository implements ArticleRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcTemplateArticleRepository(DataSource datasource) {
+        this.jdbcTemplate = new JdbcTemplate(datasource);
+    }
+
+    @Override
+    public Long save(Article article) {
+        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
+        jdbcInsert.withTableName("article").usingGeneratedKeyColumns("id");
+
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("writer", article.getWriter());
+        parameters.put("title", article.getTitle());
+        parameters.put("content", article.getContent());
+
+        Number key = jdbcInsert.executeAndReturnKey(new MapSqlParameterSource(parameters));
+        article.setArticleId(key.longValue());
+        return article.getArticleId();
+    }
+
+    @Override
+    public Optional<Article> findById(Long articleId) {
+        List<Article> result =
+                jdbcTemplate.query("select * from article where id = ?", articleRowMapper(), articleId);
+        return result.stream().findAny();
+    }
+
+    @Override
+    public List<Article> findAll() {
+        return jdbcTemplate.query("select * from article", articleRowMapper());
+    }
+
+    private RowMapper<Article> articleRowMapper() {
+        return (rs, rowNum) -> {
+            Article article = new Article(
+                    rs.getString("writer"),
+                    rs.getString("title"),
+                    rs.getString("content")
+            );
+            article.setArticleId(rs.getLong("id"));
+            return article;
+        };
+    }
+
+}
---
+++ src/main/java/codesquad/codestagram/user/repository/impl/JdbcTemplateUserRepository.java
@@ -0,0 +1,76 @@
+package codesquad.codestagram.user.repository.impl;
+
+import codesquad.codestagram.user.domain.User;
+import codesquad.codestagram.user.repository.UserRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class JdbcTemplateUserRepository implements UserRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcTemplateUserRepository(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public Long save(User user) {
+        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
+        jdbcInsert.withTableName("member").usingGeneratedKeyColumns("seq");
+
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("userId", user.getUserId());
+        parameters.put("password", user.getPassword());
+        parameters.put("name", user.getName());
+        parameters.put("email", user.getEmail());
+
+        Number key = jdbcInsert.executeAndReturnKey(new MapSqlParameterSource(parameters));
+        user.setSeq(key.longValue());
+        return user.getSeq();
+    }
+
+    @Override
+    public Optional<User> findBySeq(Long seq) {
+        List<User> result = jdbcTemplate.query("select * from member where seq = ?", userRowMapper(), seq);
+        return result.stream().findAny();
+    }
+
+    @Override
+    public List<User> findAll() {
+        return jdbcTemplate.query("select * from member", userRowMapper());
+    }
+
+    @Override
+    public Long update(User updatedUser) {
+        String sql = "update member set userId = ?, password = ?, name = ?, email = ? where seq = ?";
+        int rowsAffected = jdbcTemplate.update(sql,
+                updatedUser.getUserId(),
+                updatedUser.getPassword(),
+                updatedUser.getName(),
+                updatedUser.getEmail(),
+                updatedUser.getSeq()
+                );
+        return (rowsAffected > 0) ? updatedUser.getSeq() : null;
+    }
+
+    private RowMapper<User> userRowMapper() {
+        return (rs, rowNum) -> {
+            User user = new User(
+                    rs.getString("userId"),
+                    rs.getString("password"),
+                    rs.getString("name"),
+                    rs.getString("email")
+            );
+            user.setSeq(rs.getLong("seq"));
+            return user;
+        };
+    }
+}
