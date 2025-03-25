42fdb19138e36b1556e6517dede3d65df2edd723
jaehoonchoi <eeoos8840@gmail.com>
2025-03-25 20:17:01 +0900

    Feat(ArticleRequest): toEntity 메서스 생성과 그에 따른 controller 변경

--- src/main/java/codesquad/codestagram/article/dto/ArticleRequest.java
+++ src/main/java/codesquad/codestagram/article/dto/ArticleRequest.java
@@ -1,8 +1,18 @@
 package codesquad.codestagram.article.dto;
 
+import codesquad.codestagram.article.domain.Article;
+
 public record ArticleRequest(
         String writer,
         String title,
         String contents
 ) {
+
+    public Article toEntity() {
+        return new Article(
+                writer,
+                title,
+                contents
+        );
+    }
 }
