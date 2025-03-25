4d3376540c81d4055131a3ceed833e5bb625e33b
jaehoonchoi <eeoos8840@gmail.com>
2025-03-25 17:52:43 +0900

    Feat(JdbcUserRepository): jdbc로 구현한 UserRepository 구현체 생성

--- src/main/java/codesquad/codestagram/user/repository/impl/JdbcUserRepository.java
+++ src/main/java/codesquad/codestagram/user/repository/impl/JdbcUserRepository.java
@@ -1,2 +1,183 @@
-package codesquad.codestagram.user.repository.impl;public class JdbcUserRepository {
+package codesquad.codestagram.user.repository.impl;
+
+import codesquad.codestagram.user.domain.User;
+import codesquad.codestagram.user.repository.UserRepository;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class JdbcUserRepository implements UserRepository {
+
+    private final DataSource dataSource;
+
+    public JdbcUserRepository(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public Long save(User user) {
+        String sql = "insert into member(userId, password, name, email) values(?, ?, ?, ?)";
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            pstmt.setString(1, user.getUserId());
+            pstmt.setString(2, user.getPassword());
+            pstmt.setString(3, user.getName());
+            pstmt.setString(4, user.getEmail());
+
+            pstmt.executeUpdate();
+            rs = pstmt.getGeneratedKeys();
+
+            if (rs.next()) {
+                user.setSeq(rs.getLong(1));
+            } else {
+                throw new SQLException("id 조회 실패");
+            }
+            return user.getSeq();
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }finally {
+            close(conn, pstmt, rs);
+        }
+    }
+
+    @Override
+    public Optional<User> findBySeq(Long seq) {
+        String sql = "select * from member where seq = ?";
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setLong(1, seq);
+
+            rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                User user = new User(
+                        rs.getString("userId"),
+                        rs.getString("password"),
+                        rs.getString("name"),
+                        rs.getString("email")
+                );
+                user.setSeq(seq);
+
+                return Optional.of(user);
+            }
+            return Optional.empty();
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }finally {
+            close(conn, pstmt, rs);
+        }
+    }
+
+    @Override
+    public List<User> findAll() {
+        String sql = "select * from member";
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+
+            rs = pstmt.executeQuery();
+
+            List<User> users = new ArrayList<>();
+            while (rs.next()) {
+                User user = new User(
+                        rs.getString("userId"),
+                        rs.getString("password"),
+                        rs.getString("name"),
+                        rs.getString("email")
+                );
+
+                user.setSeq(rs.getLong("seq"));
+                users.add(user);
+            }
+            return users;
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        } finally{
+            close(conn, pstmt, rs);
+        }
+    }
+
+    @Override
+    public Long update(User updatedUser) {
+        String sql = "update member set userId = ?, password = ?, name = ?, email = ? WHERE seq = ?";
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+
+            pstmt.setString(1, updatedUser.getUserId());
+            pstmt.setString(2, updatedUser.getPassword());
+            pstmt.setString(3, updatedUser.getName());
+            pstmt.setString(4, updatedUser.getEmail());
+            pstmt.setLong(5, updatedUser.getSeq());
+
+            int affectedRows = pstmt.executeUpdate();
+            if (affectedRows == 0) {
+                throw new SQLException("업데이트할 유저를 찾을 수 없음: " + updatedUser.getSeq());
+            }
+            return updatedUser.getSeq();
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }finally {
+            close(conn, pstmt, null);
+        }
+    }
+
+    private Connection getConnection() {
+        return DataSourceUtils.getConnection(dataSource);
+    }
+
+    private void close(Connection conn, PreparedStatement pstmt, ResultSet rs) {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            if (pstmt != null) {
+                pstmt.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (conn != null) {
+                close(conn);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void close(Connection conn) throws SQLException {
+        DataSourceUtils.releaseConnection(conn, dataSource);
+    }
 }
