c005f7dc6973a55ae1437802b671af27ae8593d4
jaehoonchoi <eeoos8840@gmail.com>
2025-03-25 15:11:31 +0900

    Feat(SignUpRequest, UserUpdateRequest): toEntity 메서드 생성

--- src/main/java/codesquad/codestagram/user/dto/UserUpdateRequest.java
+++ src/main/java/codesquad/codestagram/user/dto/UserUpdateRequest.java
@@ -1,9 +1,16 @@
 package codesquad.codestagram.user.dto;
 
+import codesquad.codestagram.user.domain.User;
+
 public record UserUpdateRequest(
         String userId,
         String password,
         String name,
         String email
 ) {
+    public User toEntity(Long userSeq) {
+        User user = new User(userId, password, name, email);
+        user.setSeq(userSeq);
+        return user;
+    }
 }
